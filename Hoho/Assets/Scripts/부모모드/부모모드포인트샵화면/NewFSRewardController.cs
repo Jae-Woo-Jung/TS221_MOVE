8f8d374050fde860f13dfe933aca3c90b43d852d
2cm <leecm0127@gmail.com>
2022-11-23 02:20:01 +0900

    시작화면 및 칭찬카드도움말 화면 해상도 조정, 부모모드보상목록 서버에 보내는 스크립트 작성

---
+++ Hoho/Assets/Scripts/부모모드/부모모드포인트샵화면/NewFSRewardController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Firebase.Firestore;
+using Firebase.Extensions;
+
+public class NewFSRewardController : MonoBehaviour
+{
+    public TMP_InputField titleInput;
+    public TMP_InputField contentInput;
+    public TMP_InputField pointInput;
+    // Start is called before the first frame update
+    public void SendRewardList()
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("ParentUsers").Document("001").Collection("Point").Document("RewardList_1");
+
+
+        Dictionary<string, object> RewardList = new Dictionary<string, object>
+        {
+            { "제목", titleInput.text },
+            { "내용", contentInput.text },
+            { "포인트", System.Int32.Parse(pointInput.text) },
+            { "완성여부", false }, //AddRewardController에 완성여부관련 변수 추가해야 할 듯.
+            { "type", "list" }
+        };
+        docRef.SetAsync(RewardList).ContinueWithOnMainThread(task => {
+            Debug.Log("Added data to the RewardList document in the Point collection.");
+        });
+        titleInput.text = "";
+        contentInput.text = "";
+        pointInput.text = "";
+    }
+
+
+    public void newAddReward()
+    {
+        
+        SendRewardList();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
